a67c77dddcddb68bc133e7205343aba582f444e3
Shouvik Mukhopadhyay <mukhopadhyay.shouvik24@gmail.com>
2023-03-28 00:34:32 +0530

    Added all the Code Files and Assets Folders

---
+++ src/components/Tours.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">Plan with Shouvik</h2>
+      </div>
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Tours;
